5cd03c646b985b53d4619bdd6807d9aeb29ee5d1
bandiliswaroop <124854944+bandiliswaroop@users.noreply.github.com>
2024-09-25 22:06:28 +0530

    Update 28.Diffie-Hellman protocol.c

--- 28.Diffie-Hellman protocol.c
+++ 28.Diffie-Hellman protocol.c
@@ -1,60 +1,43 @@
 #include <stdio.h>
-#include <math.h>
-int main()
-{
-	int q,alpha,a,b,x1,x2,y1,y2,i,j,h,k1,k2,e;
-	printf("Enter the q value: ");
-	scanf("%d",&q);
-	printf("Enter alpha value: ");
-	scanf("%d",&alpha);
-	printf("Enter a value: ");
-	scanf("%d",&a);
-	printf("Enter b value: ");
-	scanf("%d",&b);
-	if(a<q)
-	{
-		i=pow(alpha,a);
-		y1=i%q;
-	}
-	else
-	{
-		printf("invalid");
-	}
-	printf("\nThe y1 value: %d",y1);
-	if(b<q)
-	{
-		j=pow(alpha,b);
-		y2=j%q;
-	}
-	else
-	{
-		printf("invalid");
-	}
-	printf("\nThe y2 value: %d",y2);
-	h=pow(y2,a);
-	k1=h%q;
-	printf("\nThe key1 is %d",k1);
-	e=pow(y1,b);
-	k2=e%q;
-	printf("\nThe key2 is %d",k2);
-	if(k1=k2)
-	{
-		printf("\nkey is valid");
-	}
-	else
-	{
-		printf("\nkey is Invalid");
-	}
-	return 0;
+
+long long int power(int a, int b, int mod) {
+    long long int t;
+    if (b == 0) return 1; // a^0 = 1
+    if (b == 1) return a % mod; // a^1 = a
+    t = power(a, b / 2, mod);
+    if (b % 2 == 0)
+        return (t * t) % mod; // Even exponent
+    else
+        return (((t * t) % mod) * a) % mod; // Odd exponent
 }
-output:
-Enter the q value: 3
-Enter alpha value: 9
-Enter a value: 7
-Enter b value: 1
-invalid
-The y1 value: 0
-The y2 value: 0
-The key1 is 0
-The key2 is 0
-key is Invalid
+
+int main() {
+    int n, g, x, a, y, b;
+
+    printf("Enter the value of n and g: ");
+    scanf("%d %d", &n, &g);
+
+    printf("Enter the value of x for the first person: ");
+    scanf("%d", &x);
+    a = power(g, x, n); // Public key for person 1
+
+    printf("Enter the value of y for the second person: ");
+    scanf("%d", &y);
+    b = power(g, y, n); // Public key for person 2
+
+    // Compute the shared secret keys
+    long long int key1 = power(b, x, n); // Key for person 1
+    long long int key2 = power(a, y, n); // Key for person 2
+
+    printf("Key for the first person is: %lld\n", key1);
+    printf("Key for the second person is: %lld\n", key2);
+
+    return 0;
+}
+
+/*Enter the value of n and g: 23 5
+Enter the value of x for the first person: 6
+Enter the value of y for the second person: 15
+	Key for the first person is: 2
+Key for the second person is: 2
+*/
